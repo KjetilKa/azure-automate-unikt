06ab9a4aef89d32b6c94a4a829d10261324a6f9c
Kjetil Karlsen <kjetil.karlsen@nav.no>
2022-05-10 14:43:23 +0200

    gjort oppgave 2, 3 og 4

---
+++ src/oppgave_3.ps1
@@ -0,0 +1,19 @@
+$ErrorActionPreference = 'Stop'
+$webRequest = Invoke-WebRequest -Uri "http://nav-deckofcards.herokuapp.com/shuffle"
+$kortstokkJson = $webRequest.Content
+$kortstokk = ConvertFrom-Json -InputObject $kortstokkJson
+
+function kortstokkTilStreng  {
+    [OutputType([string])]
+    param (
+        [object[]]
+        $kortstokk
+    )
+    $streng = ""
+    foreach ($kort in $kortstokk) {
+        $streng = $streng + "$($kort.suit[0])" + "$($kort.value)" + ","
+    }
+    return $streng
+}
+
+Write-Output "Kortstokk: $(kortstokkTilStreng -kortstokk $kortstokk)"
