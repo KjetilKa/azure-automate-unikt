8b072c38ac24dc14a1eb385796d902766fa9750f
Kjetil Karlsen <Kjetil.Karlsen@nav.no>
2022-05-13 14:12:09 +0200

    gjort oppgave 10

---
+++ src/oppgave_10.ps1
@@ -0,0 +1,104 @@
+$ErrorActionPreference = 'Stop'
+$webRequest = Invoke-WebRequest -Uri "http://nav-deckofcards.herokuapp.com/shuffle"
+$kortstokkJson = $webRequest.Content
+$kortstokk = ConvertFrom-Json -InputObject $kortstokkJson
+
+function kortstokkTilStreng  {
+    [OutputType([string])]
+    param (
+        [object[]]
+        $kortstokk
+    )
+    $streng = ""
+    foreach ($kort in $kortstokk) {
+        $streng = $streng + "$($kort.suit[0])" + "$($kort.value)" + ","
+    }
+    return $streng
+}
+
+Write-Output "Kortstokk: $(kortstokkTilStreng -kortstokk $kortstokk)"
+
+function sumPoengKortstokk {
+    [OutputType([int])]
+    param (
+        [object[]]
+        $kortstokk
+    )
+
+    $poengKortstokk = 0
+
+    foreach ($kort in $kortstokk) {
+        $poengKortstokk += switch ($kort.value) {
+            { $_ -cin @('J', 'Q', 'K') } { 10 }
+            'A' { 11 }
+            default { $kort.value }
+        }
+    }
+    return $poengKortstokk
+}
+
+Write-Output "Poengsum: $(sumPoengKortstokk -kortstokk $kortstokk)"
+
+
+$meg = $kortstokk[0..1]
+$kortstokk = $kortstokk[2..($kortstokk.Count - 1)]
+
+$magnus = $kortstokk[0..1]
+$kortstokk = $kortstokk[2..($kortstokk.Count - 1)]
+
+Write-Host "Meg: $(kortstokkTilStreng -kortstokk $meg)"
+Write-Host "Magnus: $(kortstokkTilStreng -kortstokk $magnus)"
+Write-Host "Kortstokk: $(kortstokkTilStreng -kortstokk $kortstokk)"
+
+function skrivUtResultat {
+    param (
+        [string]
+        $vinner,        
+        [object[]]
+        $kortStokkMagnus,
+        [object[]]
+        $kortStokkMeg        
+    )
+    Write-Output "Vinner: $vinner"
+    Write-Output "Magnus | $(sumPoengKortstokk -kortstokk $kortStokkMagnus) | $(kortstokkTilStreng -kortstokk $kortStokkMagnus)"    
+    Write-Output "Meg    | $(sumPoengKortstokk -kortstokk $kortStokkMeg) | $(kortstokkTilStreng -kortstokk $kortStokkMeg)"
+}
+
+# bruker 'blackjack' som et begrep - er 21
+$blackjack = 21
+
+if ((sumPoengKortstokk -kortstokk $meg) -eq $blackjack -and (sumPoengKortstokk -kortstokk $magnus) -eq $blackjack ) {
+    skrivUtResultat -vinner "Uavgjort" -kortStokkMagnus $magnus -kortStokkMeg $meg
+    exit
+}
+elseif ((sumPoengKortstokk -kortstokk $meg) -eq $blackjack) {
+    skrivUtResultat -vinner "Meg" -kortStokkMagnus $magnus -kortStokkMeg $meg
+    exit
+}
+elseif ((sumPoengKortstokk -kortstokk $magnus) -eq $blackjack) {
+    skrivUtResultat -vinner "Magnus" -kortStokkMagnus $magnus -kortStokkMeg $meg
+    exit
+}
+
+while ((sumPoengKortstokk -kortstokk $meg) -lt 17) {
+    $meg += $kortstokk[0]
+    $kortstokk = $kortstokk[1..($kortstokk.Count - 1)]
+}
+
+if ((sumPoengKortstokk -kortstokk $meg) -gt $blackjack) {
+    skrivUtResultat -vinner "Magnus" -kortStokkMagnus $magnus -kortStokkMeg $meg
+    exit
+}
+
+while ((sumPoengKortstokk -kortstokk $magnus) -le (sumPoengKortstokk -kortstokk $meg)) {
+    $magnus += $kortstokk[0]
+    $kortstokk = $kortstokk[1..($kortstokk.Count - 1)]
+}
+
+### Magnus taper spillet dersom poengsummen er høyere enn 21
+if ((sumPoengKortstokk -kortstokk $magnus) -gt $blackjack) {
+    skrivUtResultat -vinner "Meg" -kortStokkMagnus $magnus -kortStokkMeg $meg
+    exit
+}
+
+skrivUtResultat -vinner "Magnus" -kortStokkMagnus @magnus -kortStokkMeg $meg
